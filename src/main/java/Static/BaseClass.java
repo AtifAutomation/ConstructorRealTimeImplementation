128780c7c42ab16ab962d58f4e9a01c94efc571a
Atif <atifhossain99@gmail.com>
2024-01-31 20:23:00 +0530

    Static Concept code

---
+++ src/main/java/Static/BaseClass.java
@@ -0,0 +1,15 @@
+package Static;
+
+public class BaseClass {
+    public static void main(String[] args) {
+        StaticVaribaleMethod.num=190;
+        StaticVaribaleMethod.staticMethod();
+        System.out.println(StaticVaribaleMethod.num);
+        StaticVaribaleMethod obj1=new StaticVaribaleMethod();
+        StaticVaribaleMethod obj2=new StaticVaribaleMethod();
+        System.out.println("****** static variable and method will have one copy in memory ******");
+        System.out.println(obj1.num);
+        System.out.println(obj2.num);
+
+    }
+}
