f5a2a557db59ec7e2f034ab02482df5803b09b44
Atif <atifhossain99@gmail.com>
2024-01-31 21:40:50 +0530

    Magic method :main

---
+++ src/main/java/MagicMethod/mainMethod.java
@@ -0,0 +1,19 @@
+package MagicMethod;
+
+import java.sql.SQLOutput;
+
+public class mainMethod {
+    public static void main(String[] args) {
+        System.out.println("Argument passed are :");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+//    public static void method(String[] args) {
+//        System.out.println("Argument passed are :");
+//        for (String arg : args) {
+//            System.out.println(arg);
+//        }
+//    }
+}
