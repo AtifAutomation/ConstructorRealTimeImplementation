04c24d1b714bd4387aca1816576d8815a88ac7c9
Atif <atifhossain99@gmail.com>
2024-01-31 21:40:14 +0530

    Static Concept code

--- src/main/java/Static/StaticVaribaleMethod.java
+++ src/main/java/Static/StaticVaribaleMethod.java
@@ -4,8 +4,7 @@ public class StaticVaribaleMethod {
 
     public static int num;
 
-    public static void staticMethod()
-    {
+    public static void staticMethod() {
         System.out.println("Static method");
     }
 
