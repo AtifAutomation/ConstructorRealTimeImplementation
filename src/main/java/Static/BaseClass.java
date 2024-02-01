974a0593f473619381d2c58feb916fb02786af62
Atif <atifhossain99@gmail.com>
2024-02-01 06:36:01 +0530

    Static Real World Implementation

--- src/main/java/Static/BaseClass.java
+++ src/main/java/Static/BaseClass.java
@@ -1,15 +1,27 @@
 package Static;
 
+import org.openqa.selenium.WebDriver;
+
 public class BaseClass {
+    public static String url="https://www.geeksforgeeks.org/constructors-in-java/";
+    public static WebDriver driver;
     public static void main(String[] args) {
-        StaticVaribaleMethod.num = 190;
-        StaticVaribaleMethod.staticMethod();
-        System.out.println(StaticVaribaleMethod.num);
-        StaticVaribaleMethod obj1 = new StaticVaribaleMethod();
-        StaticVaribaleMethod obj2 = new StaticVaribaleMethod();
-        System.out.println("****** static variable and method will have one copy in memory ******");
-        System.out.println(obj1.num);
-        System.out.println(obj2.num);
+//        StaticVaribaleMethod.num = 190;
+//        StaticVaribaleMethod.staticMethod();
+//        System.out.println(StaticVaribaleMethod.num);
+//        StaticVaribaleMethod obj1 = new StaticVaribaleMethod();
+//        StaticVaribaleMethod obj2 = new StaticVaribaleMethod();
+//        System.out.println("****** static variable and method will have one copy in memory ******");
+//        System.out.println(obj1.num);
+//        System.out.println(obj2.num);
+        RealTimeImplementation obj1=new RealTimeImplementation(driver,url);
+        RealTimeImplementation.setup();
+        RealTimeImplementation.tearDown();
+
+
+
+
+
 
     }
 }
