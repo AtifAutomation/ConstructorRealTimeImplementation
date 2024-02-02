a157c9ac4c5c90c818621df8667f565ac6f661a8
Atif <atifhossain99@gmail.com>
2024-02-02 07:09:46 +0530

    String

---
+++ src/main/java/Stringg/Stringgg.java
@@ -0,0 +1,14 @@
+package Stringg;
+
+import java.util.Locale;
+
+public class Stringgg {
+    public static void main(String[] args) {
+        String str=new String("Akhil");
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.toLowerCase());
+        System.out.println(str.substring(1,4));
+    }
+}
